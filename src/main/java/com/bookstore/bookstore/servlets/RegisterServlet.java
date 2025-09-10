8ad04c64e0d045740c118c40afb9316502224fab
Timothy Miamba <timal0361@gmail.com>
2025-09-10 14:00:04 +0200

    Add registration backend logic

--- src/main/java/com/bookstore/bookstore/servlets/RegisterServlet.java
+++ src/main/java/com/bookstore/bookstore/servlets/RegisterServlet.java
@@ -1,6 +1,33 @@
 package com.bookstore.bookstore.servlets;
 
+import com.bookstore.bookstore.controller.UserController;
+import com.bookstore.bookstore.model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebServlet("/signup")
 public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String role =  req.getParameter("role");
+
+        UserController userController = new UserController();
+        User user = new User(username, password, role);
+        userController.saveUser(user);
+        req.getSession().setAttribute("userId", user.getId());
+        req.getSession().setAttribute("username", username);
+        req.getSession().setAttribute("role", role);
+        if (user.getRole().equals("admin")) {
+            resp.sendRedirect("admin.jsp");
+        } else {
+            resp.sendRedirect("home.jsp");
+        }
+    }
 }
